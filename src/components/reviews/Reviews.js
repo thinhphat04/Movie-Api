c4a09f4e926e2c7fca0d4448dd3eafb701ca1fca
ngothinhphat <ngothinhphat244@gmail.com>
2023-12-16 22:06:55 +0700

    update client

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,95 +1,76 @@
-import {useEffect, useRef} from 'react';
-import api from '../../api/axiosConfig';
-import {useParams} from 'react-router-dom';
-import {Container, Row, Col} from 'react-bootstrap';
-import ReviewForm from '../reviewForm/ReviewForm';
+import { useEffect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
 
-import React from 'react'
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+  const revText = useRef();
+  let params = useParams();
+  const movieId = params.movieId;
 
-const Reviews = ({getMovieData,movie,reviews,setReviews}) => {
+  useEffect(() => {
+    getMovieData(movieId);
+  }, [getMovieData, movieId]);
 
-    const revText = useRef();
-    let params = useParams();
-    const movieId = params.movieId;
+  const addReview = async (e) => {
+    e.preventDefault();
+    const rev = revText.current;
 
-    useEffect(()=>{
-        getMovieData(movieId);
-    },[])
+    try {
+      const response = await api.post("/api/v1/reviews", {
+        reviewBody: rev.value,
+        imdbId: movieId,
+      });
 
-    const addReview = async (e) =>{
-        e.preventDefault();
+      const updatedReviews = [...reviews, { body: rev.value }];
+      rev.value = "";
 
-        const rev = revText.current;
-
-        try
-        {
-            const response = await api.post("/api/v1/reviews",{reviewBody:rev.value,imdbId:movieId});
-
-            const updatedReviews = [...reviews, {body:rev.value}];
-    
-            rev.value = "";
-    
-            setReviews(updatedReviews);
-        }
-        catch(err)
-        {
-            console.error(err);
-        }
-        
-
-
-
+      setReviews(updatedReviews);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
   return (
     <Container>
-        <Row>
-            <Col><h3>Reviews</h3></Col>
-        </Row>
-        <Row className="mt-2">
-            <Col>
-                <img src={movie?.poster} alt="" />
-            </Col>
-            <Col>
-                {
-                    <>
-                        <Row>
-                            <Col>
-                                <ReviewForm handleSubmit={addReview} revText={revText} labelText = "Write a Review?" />  
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <hr />
-                            </Col>
-                        </Row>
-                    </>
-                }
-                {
-                    reviews?.map((r) => {
-                        return(
-                            <>
-                                <Row>
-                                    <Col>{r.body}</Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <hr />
-                                    </Col>
-                                </Row>                                
-                            </>
-                        )
-                    })
-                }
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <hr />
-            </Col>
-        </Row>        
+      <Row>
+        <Col>
+          <h3>Reviews</h3>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col>
+          <img src={movie?.poster} alt="" />
+        </Col>
+        <Col>
+          <ReviewForm
+            handleSubmit={addReview}
+            revText={revText}
+            labelText="Write a Review?"
+          />
+          <hr />
+          {reviews?.map((r, index) => (
+            <div key={index}>
+              <Row>
+                <Col>{r.body}</Col>
+              </Row>
+              <Row>
+                <Col>
+                  <hr />
+                </Col>
+              </Row>
+            </div>
+          ))}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
